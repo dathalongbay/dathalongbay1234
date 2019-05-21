304a8db873c50518dbc42969730fbcd2878134ed
dathalongbay <dathalongbay@gmail.com>
2019-05-21 19:23:31 +0700

    111

--- index.php
+++ index.php
@@ -9,19 +9,26 @@
 </head>
 <body>
 
-<h1>Tính chỉ số BMI</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Tính chỉ số BMI</h1>
 
-<form name="bmi" action="" method="post">
-    <div class="form-group">
-        <label >Chiều cao ( cm )</label>
-        <input type="text" class="form-control" name="height" value="">
+            <form name="bmi" action="" method="post">
+                <div class="form-group">
+                    <label >Chiều cao ( cm )</label>
+                    <input type="text" class="form-control" name="height" value="">
+                </div>
+                <div class="form-group">
+                    <label >Cân nặng ( kg ) </label>
+                    <input type="text" class="form-control" name="weight">
+                </div>
+                <button type="submit" name="calc" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label >Cân nặng ( kg ) </label>
-        <input type="text" class="form-control" name="weight">
-    </div>
-    <button type="submit" name="calc" class="btn btn-primary">Submit</button>
-</form>
+</div>
+
 
 
 
