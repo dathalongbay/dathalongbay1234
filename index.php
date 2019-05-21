866276f9d25064fd321ce97a12528d595fbbe52b
dathalongbay <dathalongbay@gmail.com>
2019-05-21 19:21:44 +0700

    111

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+
+<h1>Tính chỉ số BMI</h1>
+
+<form name="bmi" action="" method="post">
+    <div class="form-group">
+        <label >Chiều cao ( cm )</label>
+        <input type="text" class="form-control" name="height" value="">
+    </div>
+    <div class="form-group">
+        <label >Cân nặng ( kg ) </label>
+        <input type="text" class="form-control" name="weight">
+    </div>
+    <button type="submit" name="calc" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+</body>
+</html>
